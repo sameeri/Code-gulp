f3441ff14c041aa44d83a4bb46df3fd1b2e7aabd
Sameeri Marryboyina <smarryboyina@telogical.com>
2015-07-16 19:19:01 -0700

    Understanding gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
 var gulp = require('gulp');
+//
+//console.log('gulp:', gulp);
+//
+//console.log('gulp - constructor', gulp.constructor);
+//console.log('gulp - constructor prototype', gulp.constructor.prototype);
 
-console.log('gulp:', gulp);
+gulp.task('default', function doSomething(){
+  console.log("This is default. I am important for gulp tried to find me if you don't specify a task!");
+});
+
+gulp.task('Awesome', function (){
+  console.log('Taking gulp awesomeness in steps!');
+});
+
---
+++ index.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+
+console.log('gulp:', gulp);
+
+console.log('gulp - constructor', gulp.constructor);
+console.log('gulp - constructor prototype', gulp.constructor.prototype);

266369e06bf11010dde781e6e378666f0ef98729
Sameeri Marryboyina <smarryboyina@telogical.com>
2015-07-17 10:00:55 -0700

    adding sequence based tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,47 @@
 var gulp = require('gulp');
-//
-//console.log('gulp:', gulp);
-//
-//console.log('gulp - constructor', gulp.constructor);
-//console.log('gulp - constructor prototype', gulp.constructor.prototype);
+var runSequence = require('run-sequence');
 
-gulp.task('default', function doSomething(){
-  console.log("This is default. I am important for gulp tried to find me if you don't specify a task!");
+gulp.task('Cut Onions', function(){
+    console.log('Cutting oninons!');
 });
 
-gulp.task('Awesome', function (){
-  console.log('Taking gulp awesomeness in steps!');
+
+gulp.task('Cut Tomatoes', function(){
+    console.log('Cutting tomatoes!');
 });
 
+gulp.task('Cut Peppers', function(){
+    console.log('Cutting peppers!');
+});
+
+gulp.task('Scoop Avacado', function(){
+    console.log('Avacadooo!');
+});
+
+gulp.task('Mash n Mix', function(){
+    console.log('Mix everything!');
+});
+
+
+gulp.task('Add Cilantro', function(){
+    console.log('Add some flavor!');
+});
+
+
+gulp.task('Add gauc mix', function(){
+      console.log('Some spicy gaucamole mix :)');        
+});
+
+
+gulp.task('Add lime', function(){
+    console.log('Nice and sour!');
+});
+
+gulp.task('Prepare ingredients', ['Cut Onions', 'Cut Tomatoes', 'Cut Peppers', 'Scoop Avacado']);
+
+gulp.task('Add extras', ['Add Cilantro', 'Add gauc mix', 'Add lime']);
+
+gulp.task('Recipe', function (){
+    runSequence('Prepare ingredients','Mash n Mix', 'Add extras'); 
+    console.log('We have amazing gauc ready for you!');
+});
--- index.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-
-console.log('gulp:', gulp);
-
-console.log('gulp - constructor', gulp.constructor);
-console.log('gulp - constructor prototype', gulp.constructor.prototype);
